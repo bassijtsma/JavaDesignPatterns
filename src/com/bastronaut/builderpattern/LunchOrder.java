1be70ab14ebbcb51cc5d78a27fff73e8445c8272
Bas <btsijtsma@gmail.com>
2016-01-12 22:51:00 +0100

    added builder pattern implementation and construction

---
+++ src/com/bastronaut/builderpattern/LunchOrder.java
@@ -0,0 +1,84 @@
+package com.bastronaut.builderpattern;
+
+/**
+ * Created by BSijtsma on 12-01-2016.
+ */
+/*
+Solves common problem of determining what constructor to use.
+Oftentime multiple constructor (telescoping) becomes confusing.
+Often made with static inner class, as to return object its building
+
+How it works: create a static class Builder.
+Within this class, 'setter' methods are added named according
+to the respective fields.
+
+Can also enforce protection by removing all setter methods.
+
+When you want to instantiate a LunchOrder, you will first have
+to instantiate the LunchOrder.Builder() class. Then, you're free
+to define as many of the available fields as you'd like, then
+call the LunchOrder.build() method.
+ */
+public class LunchOrder {
+
+    public static class Builder {
+        private String bread;
+        private String condiments;
+        private String dressing;
+        private String meat;
+
+        public Builder() { }
+
+        public LunchOrder build() {
+            return new LunchOrder(this);
+        }
+
+        public Builder bread(String bread) {
+            this.bread = bread;
+            return this;
+        }
+
+        public Builder condiments(String condiments) {
+            this.condiments = condiments;
+            return this;
+        }
+
+        public Builder dressing(String dressing) {
+            this.dressing = dressing;
+            return this;
+        }
+
+        public Builder meat(String meat) {
+            this.meat = meat;
+            return this;
+        }
+    }
+
+    private final String bread;
+    private final String condiments;
+    private final String dressing;
+    private final String meat;
+
+    public LunchOrder(Builder builder){
+        this.bread = builder.bread;
+        this.condiments = builder.condiments;
+        this.dressing = builder.dressing;
+        this.meat = builder.meat;
+    }
+
+    public String getBread() {
+        return bread;
+    }
+
+    public String getCondiments() {
+        return condiments;
+    }
+
+    public String getDressing() {
+        return dressing;
+    }
+
+    public String getMeat() {
+        return meat;
+    }
+}
--- src/com/bastronaut/builderpattern/Main.java
+++ src/com/bastronaut/builderpattern/Main.java
@@ -5,5 +5,18 @@ package com.bastronaut.builderpattern;
  */
 
 public class Main {
-    BuilderPattern builderPattern = new BuilderPattern();
+    public static void main(String[] args ) {
+        LunchOrder.Builder lunchBuilderPattern = new LunchOrder.Builder();
+        // pattern allows to define as many/little fields as you'd like,
+        // in the order that you'd like (e.g. meat is not used).
+        lunchBuilderPattern.bread("wheat").dressing("Ketchup").condiments("cheese");
+
+        LunchOrder lunchOrder = lunchBuilderPattern.build();
+
+        System.out.println(lunchOrder.getBread());
+        System.out.println(lunchOrder.getMeat());
+        System.out.println(lunchOrder.getCondiments());
+        System.out.println(lunchOrder.getDressing());
+    }
+
 }
