96fd871d738659b73031d1dec4a9c1e2ab82c927
bassijtsma <btsijtsma@gmail.com>
2016-01-17 16:31:14 +0100

    Added files for adapter pattern

---
+++ src/com/bastronaut/adapterpattern/Adapter.java
@@ -0,0 +1,7 @@
+package com.bastronaut.adapterpattern;
+
+/**
+ * Created by Bas on 17-1-2016.
+ */
+public class Adapter {
+}
---
+++ src/com/bastronaut/adapterpattern/Main.java
@@ -0,0 +1,12 @@
+package com.bastronaut.adapterpattern;
+
+/**
+ * Created by Bas on 17-1-2016.
+ */
+/*
+Think of this as a plug class. Convert an interface to another
+interface. Often useful to cover access to legacy systems.
+consists of: Client, Adapter, Adaptee
+ */
+public class Main {
+}
