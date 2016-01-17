d14c2ed07d817c28cbf2f8e2824674ba937586d0
bassijtsma <btsijtsma@gmail.com>
2016-01-17 22:06:57 +0100

    setup bridge design pattern

---
+++ src/com/bastronaut/bridgedesignpattern/Main.java
@@ -0,0 +1,18 @@
+package com.bastronaut.bridgedesignpattern;
+
+/**
+ * Created by Bas on 17-1-2016.
+ */
+/*
+Looks similar to adapter pattern. However, the bridge pattern
+is planned in advance, rather than done to deal with legacy
+code. It's meant to decouple abstraction and implementation,
+using encapsulation, composition and inheritance. It is designed
+so that changes in abstraction will not affect the client.
+This can be done when you know the details won't be right
+from the start.
+ */
+
+
+public class Main {
+}
