062da2ec9fb8c39042cb2ba758894f8d0f06c0bb
Bas <btsijtsma@gmail.com>
2016-01-19 22:16:48 +0100

    setup package and main for composite pattern

---
+++ src/com/bastronaut/compositepattern/Main.java
@@ -0,0 +1,16 @@
+package com.bastronaut.compositepattern;
+
+/**
+ * Created by BSijtsma on 19-01-2016.
+ */
+/*
+Meant to treat components the same whether it is part of a structure,
+or the whole structure itself. Done by configuring object into a tree
+structure. Once done, you can treat individual objects as composite object.
+
+advantage: apply operations to individual and composite objects the same
+way. Example: RESTful services GET operations are almost always implemented
+with the composite model
+ */
+public class Main {
+}
