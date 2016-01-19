212e2578de643ddb7d97e4cb479be40c5ceaafac
Bas <btsijtsma@gmail.com>
2016-01-19 22:24:15 +0100

    added files for composite pattern.
    going to sleep. finish tomorrow

--- src/com/bastronaut/compositepattern/Main.java
+++ src/com/bastronaut/compositepattern/Main.java
@@ -5,12 +5,24 @@ package com.bastronaut.compositepattern;
  */
 /*
 Meant to treat components the same whether it is part of a structure,
-or the whole structure itself. Done by configuring object into a tree
-structure. Once done, you can treat individual objects as composite object.
+or the whole structure itself. Once done, you can treat individual objects as composite object.
 
 advantage: apply operations to individual and composite objects the same
 way. Example: RESTful services GET operations are almost always implemented
 with the composite model
+
+The composite pattern is made using a tree structure. You have a root Component
+class node, that serves as an abstraction for all components. It declares the
+interface for objects inside the composition. A leaf represent the Leaf objects
+or nodes inside the composition. Contains all the components methods.
+
+The Composite represents a component that has children, or methods to
+manipulate the children. ALso implements all of components methods, but
+often delegates it to its children.
+
+In this example: Menu is composite, MenuItem is leaf, MenuComponent is
+the component.
  */
 public class Main {
+
 }
---
+++ src/com/bastronaut/compositepattern/Menu.java
@@ -0,0 +1,7 @@
+package com.bastronaut.compositepattern;
+
+/**
+ * Created by BSijtsma on 19-01-2016.
+ */
+public class Menu {
+}
